31b7e65ee9e89a569926d8912c0519d2eee9b911
Seth Black <sblack@.(none)>
2010-05-18 01:53:20 -0700

    Texture safety net. Now, if the requested .bmp is missing (such as in a different folder), the oops.bmp will be loaded instead and the program won't exit.
    NOTE: leave oops.bmp in the src folder. Changing its location defeats the purpose. Code WILL exit if it looks for oops.bmp and can't find it.

--- src/texture.cpp
+++ src/texture.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-unsigned int BindTextureBMP(char *fn, bool tile){
+unsigned int BindTextureBMP(const char *fn, bool tile){
 	FILE* file;
 	char temp;
 	long i;
@@ -20,6 +20,77 @@ unsigned int BindTextureBMP(char *fn, bool tile){
 	char* data;
 	
 	cerr << "Loading texture '" << fn << "'..." << endl;
+
+	if( !(file = fopen(fn, "rb")) ){
+		cerr << "ERROR: file does not exist." << endl;
+		return BindFailSafe(tile);
+		//exit(-2);
+	}
+
+	fseek(file, 14, SEEK_CUR);
+	fread(infoheader, sizeof(BITMAPINFOHEADER), 1, file);
+	cerr << "biWidth: " << infoheader->biWidth << endl;
+	cerr << "biHeight: " << infoheader->biHeight << endl;
+	cerr << "biBitCount: " << infoheader->biBitCount << endl;
+
+	if (infoheader->biBitCount != 24){
+		cerr << "ERROR: Bpp is not 24." << endl;
+		return BindFailSafe(tile);
+		//exit(-3);
+	}
+	
+	if(!(data = (char*) malloc(infoheader->biWidth * infoheader->biHeight * 3))){
+		cerr << "ERROR: malloc failed.";
+		exit(-4);
+	}
+
+	if(!(i = fread(data, infoheader->biWidth * infoheader->biHeight * 3, 1, file))){
+		cerr << "ERROR: can not read texture.";
+		exit(-5);
+	}
+
+	//bgr -> rgb
+	for(i=0; i<(infoheader->biWidth * infoheader->biHeight * 3); i+=3){
+		temp = data[i];
+		data[i] = data[i+2];
+		data[i+2] = temp;
+	}
+	
+	fclose(file);
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	if (tile == true) {
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, infoheader->biWidth, infoheader->biHeight,GL_RGB, GL_UNSIGNED_BYTE, data);
+
+	//glTexImage2D(GL_TEXTURE_2D, 0, 3, infoheader->biWidth, infoheader->biHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+	free(infoheader);
+	free(data);
+
+	return texid;
+}
+
+
+unsigned int BindFailSafe(bool tile){
+	FILE* file;
+	char temp;
+	long i;
+	unsigned int texid;
+	char *fn = "oops.bmp";
+
+	BITMAPINFOHEADER* infoheader = (BITMAPINFOHEADER*) malloc(sizeof(BITMAPINFOHEADER));
+	char* data;
+	
+	cerr << "Loading safety texture '" << fn << "'..." << endl;
 
 	if( !(file = fopen(fn, "rb")) ){
 		cerr << "ERROR: file does not exist." << endl;
@@ -76,4 +147,3 @@ unsigned int BindTextureBMP(char *fn, bool tile){
 
 	return texid;
 }
-
