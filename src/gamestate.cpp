480c16dc4627c66b1285237ecb89d1387f98c0b5
Prentice Wongvibulsin <pwongvib@vogon.csc.calpoly.edu>
2010-05-20 18:03:05 -0700

    removed unneeded files and added gamestate object (non-networked)

---
+++ src/gamestate.cpp
@@ -0,0 +1,89 @@
+
+#include "gamestate.h"
+
+gamestate::gamestate(int time) :
+_tick(time), _state(GSSTATE_INACTIVE) {
+
+}
+
+gamestate::~gamestate(){
+	_state = GSSTATE_INVALID;
+	for(vector<playerstate*>::iterator it = _enemies.begin();
+		it != _enemies.end(); it++){
+			delete (*it);
+	}
+	for(vector<objectstate*>::iterator it = _objects.begin();
+		it != _objects.end(); it++){
+			delete (*it);
+	}
+}
+
+void gamestate::draw(){
+	if(GSSTATE_ACTIVE != _state)
+		return;
+
+	//TODO: do stuff
+}
+
+void gamestate::tick(uint32_t time){
+	if(GSSTATE_ACTIVE != _state)
+		return;
+
+	uint32_t delta_time = time - _tick;
+	_tick = time;
+	
+	//TODO: do stuff
+}
+
+void gamestate::addEnemy(playerstate* e){
+	_enemies.push_back(e);
+}
+
+void gamestate::addObject(objectstate* o){
+	_objects.push_back(o);
+}
+
+playerstate::playerstate(int time):
+_tick(time), _hp(0), _mp(0),_weapon(0),
+_state(PSSTATE_INIT), _score(0){
+	for (int i=0; i<PLAYERSTATE_MAXABILITY; ++i)
+		_ability[i]=0;
+	body = bbody(_pos.x(),-_pos.y(),1,0,BB_CIRC);
+	coord2d_t dummy;
+	front = bbody(this->calcHotSpot(dummy,.6),.1,BB_CIRC);
+}
+
+playerstate::playerstate(const playerstate &player){
+	_tick = player._tick;
+	_id = player._id;
+	_hp = player._hp;
+	_mp = player._mp;
+
+	for (int i=0; i<PLAYERSTATE_MAXABILITY; ++i)
+		_ability[i]=player._ability[i];
+
+	_weapon = player._weapon;
+	_pos = player._pos;
+	_vel = player._vel;
+	_state = player._state;
+	_score = player._score;
+	body = player.body;
+	front = player.front;
+
+}
+
+coord2d_t playerstate::calcHotSpot(coord2d_t hs, double dist){
+	hs.x() = _pos.x()-(sin(_vel.x())*dist);
+	hs.y() = -_pos.y()-(cos(_vel.x())*dist);
+	return hs;
+}
+
+objectstate::objectstate():_id(0), _hp(0), _type(0){}
+
+objectstate::objectstate(const objectstate &other){
+	_id=other._id;
+	_hp=other._hp;
+	_type=other._type;
+	_pos=other._pos;
+	body=other.body;
+}
---
+++ src/gamestate.h
@@ -0,0 +1,79 @@
+
+#pragma once
+
+#include <vector>
+#include "types.h"
+#include "stdint.h"
+#include "collision.h"
+
+using namespace std;
+
+class gamestate;
+class playerstate;
+class objectstate;
+
+#define GSSTATE_INACTIVE 0
+#define GSSTATE_ACTIVE 1
+#define GSSTATE_INVALID 99
+
+class gamestate{
+public:
+	gamestate(int time);
+	~gamestate();
+
+	vector<playerstate*> _enemies;
+	vector<objectstate*> _objects;
+
+	uint32_t _tick;
+	uint8_t _state;
+
+
+	void draw();
+	void tick(uint32_t time);
+
+	void addEnemy(playerstate* e);
+	void addObject(objectstate* o);
+};
+
+#define PSSTATE_INIT 0
+#define PSSTATE_ALIVE 1
+#define PSSTATE_DEAD 2
+#define PSSTATE_WAITSYNC 3
+
+#define PLAYERSTATE_MAXABILITY 5
+
+class playerstate{
+public:
+	playerstate(int time);
+	playerstate(const playerstate& other);
+
+	uint32_t _tick; //4
+	uint16_t _id; //2
+	uint8_t _hp; //1
+	uint8_t _mp; //1
+	uint8_t _ability[PLAYERSTATE_MAXABILITY]; //1*5
+	uint8_t _weapon; //1
+	coord2d_t _pos; //8
+	coord2d_t _vel; //8  Radians
+	uint8_t _state; //1
+	uint16_t _score; //2
+	bbody body, front; //used for collision
+
+	coord2d_t calcHotSpot(coord2d_t hs, double dist);
+
+	virtual void draw() = 0;
+	virtual void tick(int time) = 0;
+};
+
+class objectstate{
+public:
+	objectstate();
+	objectstate(const objectstate& other);
+
+	uint16_t _id;
+	uint8_t _hp;
+	uint8_t _type;
+	coord2d_t _pos;
+	bbody body; //used for collision detection
+
+};
